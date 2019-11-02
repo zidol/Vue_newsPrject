1519f119bf04c3db800e67cfbb2ff147c0743ea7
zidol <ajh3166@naver.com>
2019-11-02 23:11:05 +0900

    라우터 인스턴스 등록, App.vue에 컴포넌트 등록

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    Hello
+    <tool-bar></tool-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
+import ToolBar from './components/ToolBar.vue';
 
 export default {
-  methods: {
-    fetchData() {
-      console.log('hello');
-    }
-  },
+  components:{
+    ToolBar
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  padding: 0;
+  margin : 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import {router} from './router/index.js';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router, //router : router 키 값 같으면 줄일 수있음
 }).$mount('#app')
