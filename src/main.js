ad6b10fb3bf28bf3e4d0af903db64b66d400533c
zidol <ajh3166@naver.com>
2019-11-04 22:55:49 +0900

    store 객체 생성

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import {router} from './routes/index.js';
+import { store } from './store/index.js';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   router, //router : router 키 값 같으면 줄일 수있음
+  store,
 }).$mount('#app')
