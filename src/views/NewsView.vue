68eecc666064ef8913a695aec21b0b52182dd199
zidol <ajh3166@naver.com>
2019-11-06 00:05:20 +0900

    route-link를 통해 파라미터값 붙이기

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,26 +1,39 @@
 <template>
   <div>
-    <div v-for="user in users" > {{ user.title }}</div>
+    <!-- <div v-for="user in this.$store.state.news" > 
+      {{ user.title }}
+    </div> -->
+    <p v-for="item in this.$store.state.news">
+      <a v-bind:href="item.url">{{ item.title }}</a>
+      <small>
+        {{item.time_ago}} by 
+        <!-- <router-link v-bind:to="'/user/' + item.user">{{ item.user }}</router-link> -->
+        <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
+      </small>
+    </p>
   </div>
 </template>
 
 <script>
 import {fetchNewsList} from '../api/index.js';
 export default {
-  data() {
-    return {
-      users : []
-    }
-  },
+  // data() {
+  //   return {
+  //     users : []
+  //   }
+  // },
   created() {
-    fetchNewsList()
-      .then(response => {
-        console.log(response);
-        this.users = response.data;
-      })
-      .catch(function(error) {
-        console.log(error);
-      })
+    this.$store.dispatch('FETCH_NEWS');
+    //1
+    // fetchNewsList()
+    //   .then(response => {
+    //     console.log(response);
+    //     //2
+    //     this.users = response.data;
+    //   })
+    //   .catch(function(error) {
+    //     console.log(error);
+    //   })
   }
 }
 </script>
