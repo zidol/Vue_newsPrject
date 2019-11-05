3bd335d221533e86254523b2d45311dafe8861c3
zidol <ajh3166@naver.com>
2019-11-06 00:04:23 +0900

    동적 라우트를 위한 api에 파라미터값 붙이기

--- src/api/index.js
+++ src/api/index.js
@@ -20,9 +20,14 @@ function fetchAskList() {
     return axios.get(`${config.baseUrl}ask/1.json`);
 }
 
+function fetchUserInfo(username) {
+    return axios.get(`${config.baseUrl}user/${username}.json`)
+}
+
 export {
     fetchNewsList,
     fetchJobsList,
-    fetchAskList
+    fetchAskList,
+    fetchUserInfo,
 }
 
