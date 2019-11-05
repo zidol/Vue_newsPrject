cd1e6661781b0235b3f8db04e6fc65bfc967c967
zidol <ajh3166@naver.com>
2019-11-06 00:03:07 +0900

    스토어 저장된 state 값 출력 (기본)

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,22 +1,24 @@
 <template>
     <div>
-        <div v-for="job in jobs" > {{ job.title }}</div>
+        <!-- <div v-for="job in this.$store.state.jobs" > 
+            {{ job.title }}
+        </div> -->
+        <p v-for="job in this.$store.state.jobs">
+            <a v-bind:href="job.url">{{ job.title }}</a>
+            <small>{{job.time_ago}}, {{ job.domain }}</small>
+        </p>
     </div>
 </template>
 
 <script>
-import { fetchJobsList } from '../api/index.js';
 export default {
-    data() {
-        return {
-            jobs : []
-        }
-    },
+   
     created() {
-        var vm = this;
-        fetchJobsList()
-        .then(response => this.jobs = response.data)
-        .catch(error => console.log(error));
+        // var vm = this;
+        // fetchJobsList()
+        // .then(response => this.jobs = response.data)
+        // .catch(error => console.log(error));
+        this.$store.dispatch('FETCH_JOBS');
     }
 }
 </script>
