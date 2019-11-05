c144275efcff2d92def303512971354c4304c6ed
zidol <ajh3166@naver.com>
2019-11-05 23:58:50 +0900

    getters 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList, fetchJobsList, fetchAskList} from '../api/index.js';
-
+import mutations from './mutations.js';
+import actions from './actions.js';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
@@ -9,50 +9,15 @@ export const store = new Vuex.Store({
         news : [],
         jobs : [],
         asks : [],
+        user : {},
     },
-    mutations : {
-                //(state, data)
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        },
-        SET_ASKS(state, asks) {
-            state.asks = asks;
+    //computed와 같은 속성
+    getters : {
+        fetchedAsk(state) {
+            return state.asks;
         }
     },
-    actions : {
-        //비동기 호출은 actions에서 (api호출)
-        //mutation(methods와 같은)에서 데이터를 state로 이동
-        FETCH_NEWS(context) {
-            fetchNewsList()
-                .then(response => {
-                    console.log(response.data);
-                    context.commit('SET_NEWS', response.data);//mutation에 제어권 넘김
-                })
-                .catch(error => {
-                    console.log(error);
-                    
-                });
-        },           //- ES6 Destructuring
-        FETCH_JOBS({commit}) {
-            fetchJobsList()
-                .then(({data}) => {
-                    commit('SET_JOBS', data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },         
-        FETCH_ASKS(context) {
-            fetchAskList()
-                .then(response => {
-                    context.commit('SET_ASKS', response.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        }
-    }
+    //mutations : mutations,
+    mutations,
+    actions : actions
   });
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,27 +1,37 @@
 <template>
     <div>
-        <div v-for="item in ask" >{{ item.title }}</div>
+        <p v-for="item in fetchedAsk">
+      <a v-bind:href="item.url">{{ item.title }}</a>
+      <small>{{item.time_ago}} by {{ item.user }}</small>
+    </p>
     </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index.js';
+import { mapState, mapGetters } from 'vuex'
 export default {
-    data() {
-        return {
-            ask : [],
-        }
+    computed: {
+
+        //mapHelper
+        ...mapGetters([
+            'fetchedAsk'
+        ])
+        //#3
+        // ...mapGetters({
+        //     askItems : 'fetchedAsk'
+        // })
+        //#2
+        // ...mapState({
+        //     ask : state => state.asks
+        // })
+        //#1
+        // ask(){
+        //     return this.$store.state.asks;
+        // }
     },
     // 컴토넌트 생성되자마자 시작하는 로직
     created() {
-        var vm = this;
-        fetchAskList()
-            .then(function(response) {
-                vm.ask = response.data;
-            })
-            .catch(function(error) {
-                console.log(error);
-            })
+        this.$store.dispatch('FETCH_ASKS');
     },
 }
 </script>
