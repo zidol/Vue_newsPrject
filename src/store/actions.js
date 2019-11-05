5efaf2cc280fda72c8914868f4b7848a3e663034
zidol <ajh3166@naver.com>
2019-11-05 23:56:28 +0900

    acitons, mutation 모듈화

---
+++ src/store/actions.js
@@ -0,0 +1,46 @@
+import { fetchNewsList, 
+        fetchJobsList, 
+        fetchAskList, 
+        fetchUserInfo} from '../api/index.js';
+export default {
+    //비동기 호출은 actions에서 (api호출)
+    //mutation(methods와 같은)에서 데이터를 state로 이동
+    FETCH_NEWS(context) {
+        fetchNewsList()
+            .then(response => {
+                console.log(response.data);
+                context.commit('SET_NEWS', response.data);//mutation에 제어권 넘김
+            })
+            .catch(error => {
+                console.log(error);
+                
+            });
+    },           //- ES6 Destructuring
+    FETCH_JOBS({commit}) {
+        fetchJobsList()
+            .then(({data}) => {
+                commit('SET_JOBS', data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },         
+    FETCH_ASKS({commit}) {
+        fetchAskList()
+            .then(({data}) => {
+                commit('SET_ASKS', data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+    FETCH_USER({commit}, name) {
+        fetchUserInfo(name)
+            .then(({data}) => {
+                commit('SET_USER', data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+}
---
+++ src/store/mutations.js
@@ -0,0 +1,15 @@
+export default {
+        //(state, data)
+    SET_NEWS(state, news) {
+        state.news = news;
+    },
+    SET_JOBS(state, jobs) {
+        state.jobs = jobs;
+    },
+    SET_ASKS(state, asks) {
+        state.asks = asks;
+    },
+    SET_USER(state, user) {
+        state.user = user;
+    },
+}
