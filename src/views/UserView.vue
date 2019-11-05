42775a46d7f05b2f037f6f48bb0a06cfa571a6ad
zidol <ajh3166@naver.com>
2019-11-06 00:06:22 +0900

    userview에 데이터 뿌리기
    computed를 통해 깔끔하게 바꿀수 있음

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,10 +1,24 @@
 <template>
-  
+    <div>
+        <p>name : {{ userInfo.id}}</p>
+        <p>karma : {{ userInfo.karma}}</p>
+        <p>created : {{ userInfo.created}}</p>
+        <!-- <p>{{ this.$store.state.user.about}}</p> -->
+    </div>
 </template>
 
 <script>
 export default {
-
+    computed: {
+        userInfo() {
+            return this.$store.state.user
+        }
+    },
+    created() {
+        const userName = this.$route.params.id;
+                                            //여러 데이터일 때는 객체로 넘기면됨
+        this.$store.dispatch('FETCH_USER', userName);
+    }
 }
 </script>
 
