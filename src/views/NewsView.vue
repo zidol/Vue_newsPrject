bea10607719f6f503b00777c10f99a1bccd1d368
zidol <ajh3166@naver.com>
2019-11-12 23:43:03 +0900

    css 디자인 추가

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
-    <!-- <div v-for="user in this.$store.state.news" > 
-      {{ user.title }}
-    </div> -->
-    <p v-for="item in this.$store.state.news">
-      <a v-bind:href="item.url">{{ item.title }}</a>
+    <ul class="news-list">
+      <li v-for="item in this.$store.state.news" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a v-bind:href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link class="link-text" v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
+          </small>
+        </div>
+        <!-- <a v-bind:href="item.url">
+          {{ item.title }}
+        </a>
       <small>
-        {{item.time_ago}} by 
-        <!-- <router-link v-bind:to="'/user/' + item.user">{{ item.user }}</router-link> -->
-        <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
-      </small>
-    </p>
+          {{item.time_ago}} by 
+          <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
+      </small> -->
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -38,6 +54,31 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .news-list {
+    margin : 0;
+    padding: 0;
+  }
 
+  .post {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid #eee;
+  }
+  .points {
+    width: 80px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #42b883;
+  }
+  .news-title {
+    margin:0; 
+  }
+
+  .link-text {
+    color: #828282;
+  }
 </style>
