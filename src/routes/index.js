a3e5cb179822d0ef349689b1dc379f1f7c60a7c0
zidol <ajh3166@naver.com>
2019-11-14 23:44:35 +0900

    라우트 마다 name 생성

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,15 +18,18 @@ export const router = new VueRouter({
     {
         //path : url 주소
       path : '/news',
+      name : 'news',
       //component : url 주소로 갔을 때 포시될 컴포넌트(페이지)
       component: NewsView,
     },
     {
       path : '/ask',
+      name: 'ask',
       component: AskView,
     },
     {
       path : '/jobs',
+      name: 'jobs',
       component: JobsView,
     },
     {
