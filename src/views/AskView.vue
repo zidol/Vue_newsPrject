e53b3abc144f78d12c5198291d75975414cede55
zidol <ajh3166@naver.com>
2019-11-14 23:44:56 +0900

    각 페이지마다 컴포넌트 적용

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,44 +1,53 @@
 <template>
     <div>
-        <p v-for="item in fetchedAsk">
-      <!-- <a v-bind:href="item.url">{{ item.title }}</a> -->
-      <router-link v-bind:to="`item/${item.id}`">
-          {{item.title}}
-      </router-link>
-      <small>{{item.time_ago}} by {{ item.user }}</small>
-    </p>
+      <list-item></list-item>
     </div>
+  <!-- <div>
+    <ul class="news-list">
+      <li v-for="(item, key) in fetchedAsk" class="post">
+        <div class="points">{{ item.points }}</div>
+        <div>
+          <p class="news-title">
+            <router-link v-bind:to="`item/${item.id}`">{{item.title}}</router-link>
+          </p>
+          <small class="link-text">
+            {{item.time_ago}} by {{ item.user }}a
+            <router-link class="link-text" v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div> -->
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import ListItem from '../components/ListItem.vue';
+// import { mapState, mapGetters } from "vuex";
 export default {
-    computed: {
-
-        //mapHelper
-        ...mapGetters([
-            'fetchedAsk'
-        ])
-        //#3
-        // ...mapGetters({
-        //     askItems : 'fetchedAsk'
-        // })
-        //#2
-        // ...mapState({
-        //     ask : state => state.asks
-        // })
-        //#1
-        // ask(){
-        //     return this.$store.state.asks;
-        // }
-    },
-    // 컴토넌트 생성되자마자 시작하는 로직
-    created() {
-        this.$store.dispatch('FETCH_ASKS');
-    },
+  components: {
+    ListItem,
+  },
 }
+  // computed: {
+    //mapHelper
+    // ...mapGetters(["fetchedAsk"])
+    //#3
+    // ...mapGetters({
+    //     askItems : 'fetchedAsk'
+    // })
+    //#2
+    // ...mapState({
+    //     ask : state => state.asks
+    // })
+    //#1
+    // ask(){
+    //     return this.$store.state.asks;
+    // }
+  // },
+  // 컴토넌트 생성되자마자 시작하는 로직
+  // created() {
+  //   this.$store.dispatch("FETCH_ASKS");
+  // }
+
 </script>
 
-<style>
-
-</style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,28 +1,14 @@
 <template>
-    <div>
-        <!-- <div v-for="job in this.$store.state.jobs" > 
-            {{ job.title }}
-        </div> -->
-        <p v-for="job in this.$store.state.jobs">
-            <a v-bind:href="job.url">{{ job.title }}</a>
-            <small>{{job.time_ago}}, {{ job.domain }}</small>
-        </p>
-    </div>
+  <div>
+    <list-item></list-item>
+  </div>
 </template>
 
 <script>
+import ListItem from '../components/ListItem.vue';
 export default {
-   
-    created() {
-        // var vm = this;
-        // fetchJobsList()
-        // .then(response => this.jobs = response.data)
-        // .catch(error => console.log(error));
-        this.$store.dispatch('FETCH_JOBS');
-    }
+  components: {
+    ListItem,
+  }
 }
 </script>
-
-<style>
-
-</style>
